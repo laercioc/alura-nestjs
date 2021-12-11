17d1eec4219a30e65a5743182ba86307177c53ac
Laercio Calheiros <laercio.calheiros@easycarros.com>
2021-12-11 10:48:43 -0300

    Configure sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+import { Product } from './products.model';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'alura_nestjs',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Product]),
+  ],
   controllers: [AppController, ProductsController],
   providers: [AppService, ProductService],
 })
